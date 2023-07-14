7c7130e9319b99791cf96ed72208d15761995225
Andreh Silveira <andrehbahia@gmail.com>
2023-07-14 08:51:06 -0300

    first commit

---
+++ exercicio1/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Ex 1  Crição de projeto usando o Vite</h1>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
