f295296fb32fce4fffc9f038d26fc4a52c91dd9d
Andreh Silveira <andrehbahia@gmail.com>
2023-07-14 09:28:36 -0300

    coloquei o main na raiz

--- exercicio1/App.jsx
+++ exercicio1/App.jsx
@@ -10,7 +10,6 @@ function App() {
       <div>
         <h1>Ex 1  Crição de projeto usando o Vite</h1>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
