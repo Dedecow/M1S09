c48c04d4f55448423e93482e3d30858051064ce4
Andreh Silveira <andrehbahia@gmail.com>
2023-07-15 09:42:22 -0300

    imput para receber texto

--- exercicio-1/src/components/cardAdicionar/index.jsx
+++ exercicio-1/src/components/cardAdicionar/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
 import React, { useState } from 'react';
 
+const CardAdicionar = () => {
+  const [valor, setValor] = useState('');
 
-const cardAdicionar = () => {
+  const atualizarValor = (event) => {
+    setValor(event.target.value);
+  }
+
+  return (
     <div>
-
+      <input 
+        type="text"
+        placeholder="Adicionar tarefa"
+        value={valor} 
+        onChange={atualizarValor} 
+      />
+      <p>Tarefa</p>
     </div>
+  );
 }
 
-export default cardAdicionar;
+export default CardAdicionar;
