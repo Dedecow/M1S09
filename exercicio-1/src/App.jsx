1fdb411ca496e76e3550f79a8ed4e5f308962795
Andreh Silveira <andrehbahia@gmail.com>
2023-07-23 09:19:03 -0300

    debbug-erro-vue

--- exercicio-1/src/App.jsx
+++ exercicio-1/src/App.jsx
@@ -1,28 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import CardAdicionar from './components/cardAdicionar'
-
+import React, { useState } from 'react';
+import CardAdicionar from './components/CardAdicionar';
 
 function App() {
-
   const [listaTarefas, setListaTarefas] = useState([
-    {Id: 1, textoTarefa: "tarefa 1", finalizado: false}
+    { Id: 1, textoTarefa: "tarefa 1", finalizado: false }
   ]);
-  const addTarefa = (texto) => {
-    const novaTarefa = {Id: listaTarefas.length + 1 , textoTarefa: texto, finalizado: false}
-    setListaTarefas = (novaTarefa)
+
+  const adicionarTarefa = (texto) => {
+    const novaTarefa = { Id: listaTarefas.length + 1, textoTarefa: texto, finalizado: false };
+    setListaTarefas([...listaTarefas, novaTarefa]);
   }
+
   return (
     <>
       <div>
+        <CardAdicionar adicionarTarefa={adicionarTarefa} />
         <h1> Exercicio 4 Concluido ! ! ! </h1>
-        <CardAdicionar/>
-        
-        
       </div>
-     
     </>
   )
 }
 
-export default App
+export default App;
